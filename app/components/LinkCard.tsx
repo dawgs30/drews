faed3f1d2102d3b6d52812c428f539a6366f1b7f
dawgs30 <dblackwell3030@gmail.com>
2025-11-19 14:41:00 -0700

    bug fixed

--- app/components/LinkCard.tsx
+++ app/components/LinkCard.tsx
@@ -34,8 +34,8 @@ export default function LinkCard({ href, label, icon }: LinkCardProps) {
         viewBox="0 0 24 24"
       >
         <path
-          strokeLineCap="round"
-          strokeLineJoin="round"
+          strokeLinecap="round"
+          strokeLinejoin="round"
           strokeWidth={2}
           d="M10 6H6a2 2 0 00-2 2v10a2 2 0 002 2h10a2 2 0 002-2v-4M14 4h6m0 0v6m0-6L10 14"
         />
@@ -43,4 +43,3 @@ export default function LinkCard({ href, label, icon }: LinkCardProps) {
     </Link>
   );
 }
-
